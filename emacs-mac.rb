e608118badd245cf83c0734a0e479887d936a0e1
Xin Xu <railwaycat@gmail.com>
2012-10-02 10:02:52 -0400

    recover deps automake

--- emacs-mac.rb
+++ emacs-mac.rb
@@ -1,20 +1,19 @@
 require 'formula'
 
 class EmacsMac <Formula
-  # url 'http://ftp.gnu.org/pub/gnu/emacs/emacs-23.2.tar.bz2'
-  # md5 '057a0379f2f6b85fb114d8c723c79ce2'
   homepage 'http://www.gnu.org/software/emacs/'
 
   head 'git://github.com/railwaycat/emacs-mac-port.git'
-
+  depends_on 'automake'
+  
   def caveats
     s = ""
     s += <<-EOS.undent
 
-      This is YAMAMOTO Mitsuharu's experimental Mac port, which adds
-      some of the functionality from the old Carbon and Carbon+AppKit
-      Emacs ports to Emacs 23. After installing, see README-mac and
-      NEWS-mac in #{prefix} for the port details.
+      This is YAMAMOTO Mitsuharu's experimental "Mac port" addition to
+      GNU Emacs 24. This provides a native GUI support for Mac OS X
+      10.4 - 10.8. After installing, see README-mac and NEWS-mac
+      in #{prefix} for the port details.
 
     EOS
 
@@ -28,12 +27,12 @@ class EmacsMac <Formula
 
   def install
     args = ["--prefix=#{prefix}",
-            "--without-dbus",
             "--enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp",
-            "--infodir=#{info}/emacs"]
+            "--infodir=#{info}/emacs",
+            "--with-mac",
+            "--enable-mac-app"]
 
     # build
-    args << "--with-mac" << "--enable-mac-app"
     system "./configure", *args
     system "make"
     system "make install"
